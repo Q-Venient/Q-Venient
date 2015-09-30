91381994934ab913a70b13e23df5a1777de8e2d7
Q-Venient <larrypaul.lague@yahoo.com>
2015-09-30 15:36:16 +0800

    Updated today

--- www/js/app.js
+++ www/js/app.js
@@ -32,6 +32,16 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
+  .state('app.login', {
+    url: '/login',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/login.html',
+        controller: 'PlaylistsCtrl'
+      }
+    }
+  })
+
   .state('app.contact', {
     url: '/contact',
     views: {
